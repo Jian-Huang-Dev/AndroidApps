5db37623ca10dedbde5ed0df98acde1eaf988888
Jian Huang <johnbodun@hotmail.com>
2014-10-20 22:16:13 -0400

    Add GraphingQuadratic application

---
+++ GraphingQuadratic/gen/tinkercoder/graphingquadratic/graphingquadratic/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package tinkercoder.graphingquadratic.graphingquadratic;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
