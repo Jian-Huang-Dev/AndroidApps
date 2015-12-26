321268ee87e6d70f32d802d1ef2dafdc8b1054a7
Jian Huang <johnbodun@hotmail.com>
2015-12-25 22:45:23 -0500

    cleaning up some un-needed files from eclipse

--- GraphingQuadratic/gen/tinkercoder/graphingquadratic/graphingquadratic/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package tinkercoder.graphingquadratic.graphingquadratic;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
